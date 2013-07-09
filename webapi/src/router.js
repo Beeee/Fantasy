83d4281eb3d37ca99f6f16831721f39d5ad2748b
Roar Bjurstrøm <roar.bjurstrom@gmail.com>
2013-07-09 14:05:25 +0200

    fixed

---
+++ webapi/src/router.js
@@ -0,0 +1,88 @@
+var url = require('url');
+//var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+var services = {
+    user:  require("./user_service"),
+    userteam: require("./user_team"),
+    league: require("./league"),
+    team:  require("./team"),
+    player: require("./player")
+};
+
+function sendResults(callback, res, status, statusText, headers, result) {
+  if (typeof headers === "undefined"){ headers = {};}
+  if (typeof result === "undefined"){ result = {};}
+  
+  headers["Connection"] = "close";
+  headers["Content-Type"] = "application/json";
+  res.writeHead(status, statusText, headers);
+   if (typeof callback !== "undefined"){
+    res.end(callback + "(" + JSON.stringify(result) + ");");
+	}
+  else{
+    res.end(JSON.stringify(result));
+	}
+};
+
+
+function routeCall(req, res, body) {
+ if(typeof body === "undefined") {body = ""; }
+  // Get parameters, both from the URL and the request body
+  var urlObj = url.parse(req.url, true);
+  var params = urlObj.query;
+  params["path"] = urlObj.pathname.split("/");
+  
+  if(req.headers['authorization'] !== undefined) {
+	params['authorization'] = req.headers['authorization'];
+  }  
+  var toCall = urlObj.pathname.split("/")[1];
+  
+  if (typeof services[toCall] === "undefined") {
+	console.log(toCall+ " is undefined");
+    return sendResults(params["callback"],res, 404, "SERVICE NOT FOUND");
+  }
+  
+  if (typeof services[toCall]["dispatch"][req.method] === "undefined") {
+		console.log("Undefined service for "+toCall);
+    return sendResults(params["callback"],res, 404, "SERVICE NOT FOUND");
+	}
+  
+  services[toCall]["dispatch"][req.method](params, function(status, statusText, headers, result){
+      return sendResults(params["callback"], res, status, statusText, headers, result);
+    });
+}
+// Unexpected error catching
+process.on('uncaughtException', function(err) {
+  console.error("UNCAUGHT EXCEPTION...");
+  console.error(err);
+  console.error(err.stack);
+});
+
+/*
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+*/
+
+//https.createServer(options,function (req, res) {
+http.createServer(function (req, res) {
+  //  For PUT/POST methods, wait until the
+  //  complete request body has been read.
+  //POST OG PUT ER FOREL�PG IKKE ST�TTA :)
+  if (req.method==="POST" || req.method==="PUT") {
+    var body = "";
+    req.on("data", function(data){
+      body += data;
+    })
+
+    req.on("end", function(){
+      return routeCall(req, res, body);
+    })
+
+  } else {
+    return routeCall(req, res);
+  }
+}).listen(8888);
